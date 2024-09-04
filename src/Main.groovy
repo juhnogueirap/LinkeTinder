22db7bb3bb0bb263eaf0e05328b333e24158c447
Juliana Nogueira Peixoto <juliananpeixoto01@ufrrj.br>
2024-09-04 17:03:09 -0300

    Funcionalidade de imprimir todos funcionário e empresas pré-cadastrados (menu no terminal)

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,17 +1,48 @@
 import java.time.LocalDate
+import java.util.Scanner
 import CandidatoPF
-
-
 
 
 
 static void main(String[] args) {
 
   List <CandidatoPF> candidatos = CandidatoPF.criarCandidatosDefault()
-  candidatos.each {println it}
-  print("\n \n \n")
   List <EmpresaPJ> empresas = EmpresaPJ.criarEmpresasDefault()
-  empresas.each {println it}
+
+  int opcao = menu()
+  RealizarOpcao(opcao, candidatos, empresas)
+
 }
+
+int menu() {
+  Scanner scanner = new Scanner(System.in)
+  println("Olá! Seja bem vindo ao MVP do LinkeTinder. Informe a opção desejada: " +
+          "\n 1 - Imprimir todos os candidatos" +
+          "\n 2 - Imprimir todas as empresas" +
+          "\n 3 - Fechar ")
+  int opcao = scanner.nextInt()
+  return opcao
+
+}
+
+void RealizarOpcao(int opcao, List <CandidatoPF> candidatos, List <EmpresaPJ> empresas) {
+  switch (opcao)  {
+    case 1:
+      candidatos.each{println it}
+      break
+
+    case 2:
+      empresas.each{println it}
+      break
+
+    case 3:
+         break
+    default:
+      println("Ops! Opção inválida, tente novamente: \n")
+      break
+
+  }
+}
+
 
 //Juliana Nogueira Peixoto (juliananpeixoto01@ufrrj.br)
