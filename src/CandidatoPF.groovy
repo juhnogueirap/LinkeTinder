7bde88bb179c397eb55397930081dc95436cfc1c
Juliana Nogueira Peixoto <juliananpeixoto01@ufrrj.br>
2024-09-04 16:35:15 -0300

    Criando candidatos e empresas pré cadastrados

--- src/CandidatoPF.groovy
+++ src/CandidatoPF.groovy
@@ -3,7 +3,17 @@ import groovy.transform.ToString
 @ToString
 class CandidatoPF extends Pessoa{
 
+    String cpf;
     int idade;
+
+    String getCpf(CandidatoPF candidato) {
+        String cpf = candidato.cpf
+        return cpf
+    }
+
+    void setCpf(String cpf) {
+        this.cpf = cpf
+    }
 
     def setIdade(int idade) {
         this.idade = idade
@@ -18,7 +28,69 @@ class CandidatoPF extends Pessoa{
     }
 
     String toString() {
-        return "${super.toString()}, Competências: ${competencias.join(', ')}, Idade: ${idade}"
+        return "${super.toString()}, Competências: {${competencias.join(', ')}}, Idade: ${idade}, CPF: ${cpf}"
+    }
+
+    def static criarCandidatosDefault() {
+        CandidatoPF p1 = new CandidatoPF(
+                nome: 'Aluisio',
+                email: 'aluisio@gmail.com',
+                pais: 'Brasil',
+                estado: 'MG',
+                cep: '00.000.000',
+                descricao: 'organizado e adaptável',
+                competencias: ["Java", "Kotlin"],
+                idade: 36,
+                cpf: '000.000.000.00'
+        )
+        CandidatoPF p2 = new CandidatoPF(
+                nome: 'Beatriz',
+                email: 'beatriz@gmail.com',
+                pais: 'Brasil',
+                estado: 'RJ',
+                cep: '11.111.111',
+                descricao: 'analítica e extrovertida',
+                competencias: ["C++", "Python"],
+                idade: 25,
+                cpf: '000.000.000.01'
+        )
+        CandidatoPF p3 = new CandidatoPF(
+                nome: 'Caio',
+                email: 'caio@gmail.com',
+                pais: 'Brasil',
+                estado: 'RJ',
+                cep: '22.222.222',
+                descricao: 'Criativo e comunicativo',
+                competencias: ["Ruby", "Lua"],
+                idade: 28,
+                cpf: '000.000.000.02'
+        )
+        CandidatoPF p4 = new CandidatoPF(
+                nome: 'Daniela',
+                email: 'daniela@gmail.com',
+                pais: 'Brasil',
+                estado: 'TO',
+                cep: '33.333.333',
+                descricao: 'organizada e inovadora',
+                competencias: ["Java", "Python"],
+                idade: 30,
+                cpf: '000.000.000.03'
+        )
+        CandidatoPF p5 = new CandidatoPF(
+                nome: 'Eduardo',
+                email: 'eduardo@gmail.com',
+                pais: 'Brasil',
+                estado: 'CE',
+                cep: '44.444.444',
+                descricao: 'organizado e inovador',
+                competencias: ["Java", "Python"],
+                idade: 30,
+                cpf: '000.000.000.04'
+        )
+
+        List<CandidatoPF> candidatos = [p1, p2, p3, p4, p5]
+
+        return candidatos
     }
 
 }
--- src/EmpresaPJ.groovy
+++ src/EmpresaPJ.groovy
@@ -1,12 +1,93 @@
+import java.time.LocalDate
+import groovy.transform.ToString
+
+@ToString
 class EmpresaPJ extends Pessoa{
 
-    Date dataFundacao
+    String cnpj
+    LocalDate dataFundacao
+
+    String getCnpj(EmpresaPJ empresa) {
+        String cnpj = empresa.cnpj
+        return cnpj
+    }
+
+    void setCnpj(String cnpj) {
+        this.cnpj = cnpj
+    }
 
     Date getDataFundacao() {
         return dataFundacao
     }
 
-    void setDataFundacao(Date dataFundacao) {
+    void setDataFundacao(LocalDate dataFundacao) {
         this.dataFundacao = dataFundacao
+    }
+
+    @Override
+    public String toString() {
+        return "${super.toString()}, Competências: {${competencias.join(', ')}}, Fundação: ${dataFundacao}, CNPJ: ${cnpj}"
+    }
+
+    def static criarEmpresasDefault() {
+        EmpresaPJ e1 = new EmpresaPJ(
+                nome: 'SOLUtions',
+                email: 'solutions.solu@gmail.com',
+                pais: 'USA',
+                estado: 'Minnesota',
+                cep: '10.000.000',
+                descricao: 'soluções tecnológicas',
+                competencias: ["Python", "C#"],
+                dataFundacao: LocalDate.of(1948, 8, 2),
+                cnpj: '67.818.166/0001-90'
+        )
+        EmpresaPJ e2 = new EmpresaPJ(
+                nome: 'ObleSecurity',
+                email: 'oblesecurity.oble@gmail.com',
+                pais: 'Canada',
+                estado: 'Vancouver',
+                cep: '11.000.000',
+                descricao: 'técnicas de cybersecurity',
+                competencias: ["Linux", "Compliance"],
+                dataFundacao: LocalDate.of(2000, 3, 17),
+                cnpj: '78.331.297/0001-65'
+        )
+        EmpresaPJ e3 = new EmpresaPJ(
+                nome: 'Cortello Tech',
+                email: 'cortello.tellotech@gmail.com',
+                pais: 'Brasil',
+                estado: 'SP',
+                cep: '22.000.000',
+                descricao: 'soluções de ERP',
+                competencias: ["Engenharia de Software", "Modelagem de Software"],
+                dataFundacao: LocalDate.of(1990, 7, 20),
+                cnpj: '06.537.864/0001-23'
+        )
+        EmpresaPJ e4 = new EmpresaPJ(
+                nome: 'HealthTech',
+                email: 'healthtech.htech@gmail.com',
+                pais: 'Brasil',
+                estado: 'RS',
+                cep: '33.000.000',
+                descricao: 'soluções tecnológicas para clínicas e hospitais',
+                competencias: ["Java", "POO", "PowerBI"],
+                dataFundacao: LocalDate.of(2015, 4, 26),
+                cnpj: '48.856.276/0001-00'
+        )
+        EmpresaPJ e5 = new EmpresaPJ(
+                nome: 'QualityAssistent',
+                email: 'qualityassis.quality@gmail.com',
+                pais: 'Canada',
+                estado: 'Ontario',
+                cep: '44.000.000',
+                descricao: 'assistentes tecnológicos personalizados para empresas',
+                competencias: ["IA", "Python", "MachineLearning"],
+                dataFundacao: LocalDate.of(2015, 9, 16),
+                cnpj: '06.537.864/0001-23'
+        )
+
+        List<EmpresaPJ> empresas = [e1, e2, e3, e4, e5]
+
+        return empresas
     }
 }
